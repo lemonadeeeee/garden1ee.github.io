0829ab9554f0c39baccaf8121ad35a48ec1d56af
garden1ee <garden1ee@users.noreply.github.com>
2020-06-08 03:05:57 +0900

    home page commit

---
+++ home.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.showevents').slick({
+      accessibility:true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+    });
+    $('#left').click(function(){
+        $('.showevents').slick('slickPrev');
+    });
+    $('#right').click(function(){
+        $('.showevents').slick('slickNext');
+    })
+  });
