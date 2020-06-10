08424549f7a3733a7cfe07db91910e421d324e49
Conatee <2771396@gmail.com>
2020-06-11 05:28:28 +0900

    show meeting details when clicking on meeting
    Minor changes in styles
    Show meeting detail when clicking on meeting in meeting-list page
    Fix adding marker on created meeting in meeting-map page
    Show meeting detail when clicking on marker in meeting-map page

--- main.js
+++ main.js
@@ -1,145 +1,147 @@
-var app = angular.module('mainnav',['ngSanitize', 'ui.bootstrap', 'ui.router', 'ngMap']);
-app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
-  $stateProvider
-    .state({
-        name: 'home',
-        url: '/',
-        views: {
-            'mainbody': {
-                templateUrl: 'home.html'
+var app = angular.module('mainnav', ['ngSanitize', 'ui.bootstrap', 'ui.router', 'ngMap']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state({
+            name: 'home',
+            url: '/',
+            views: {
+                'mainbody': {
+                    templateUrl: 'home.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'morefriends',
-        url: '/',
-        views: {
-            'mainbody': {
-                templateUrl: 'home.html'
-            },
-            'addfriends@morefriends': {
-                templateUrl: 'profiles/morefriends.html'
+        })
+        .state({
+            name: 'morefriends',
+            url: '/',
+            views: {
+                'mainbody': {
+                    templateUrl: 'home.html'
+                },
+                'addfriends@morefriends': {
+                    templateUrl: 'profiles/morefriends.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'filterfriends',
-        url: '/f',
-        views: {
-            'mainbody': {
-                templateUrl: 'home.html'
-            },
-            'addfriends@filterfriends': {
-                templateUrl: 'profiles/filterprofiles.html'
+        })
+        .state({
+            name: 'filterfriends',
+            url: '/f',
+            views: {
+                'mainbody': {
+                    templateUrl: 'home.html'
+                },
+                'addfriends@filterfriends': {
+                    templateUrl: 'profiles/filterprofiles.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'friends',
-        url: '/friends',
-        views: {
-            'mainbody': {
-                template: '<h3>friends list</h3>'
+        })
+        .state({
+            name: 'friends',
+            url: '/friends',
+            views: {
+                'mainbody': {
+                    template: '<h3>friends list</h3>'
+                }
             }
-        }
-    })
-    .state({
-        name: 'meeting-list',
-        url: '/meeting-list',
-        views: {
-            'mainbody': {
-                templateUrl: 'meeting-list.html'
+        })
+        .state({
+            name: 'meeting-list',
+            url: '/meeting-list',
+            views: {
+                'mainbody': {
+                    templateUrl: 'meeting-list.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'meeting-map',
-        url: '/meeting-map',
-        views: {
-            'mainbody': {
-                templateUrl: 'meeting-map.html'
+        })
+        .state({
+            name: 'meeting-map',
+            url: '/meeting-map',
+            views: {
+                'mainbody': {
+                    templateUrl: 'meeting-map.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'meeting-create',
-        url: '/meeting-create',
-        views: {
-            'mainbody': {
-                templateUrl: 'meeting-create.html'
+        })
+        .state({
+            name: 'meeting-create',
+            url: '/meeting-create',
+            views: {
+                'mainbody': {
+                    templateUrl: 'meeting-create.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'messages',
-        url: '/messages',
-        views: {
-            'mainbody': {
-                templateUrl: 'messages.html'
+        })
+        .state({
+            name: 'messages',
+            url: '/messages',
+            views: {
+                'mainbody': {
+                    templateUrl: 'messages.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'mypage',
-        url: '/mypage',
-        views: {
-            'mainbody': {
-                templateUrl: 'mypage.html'
+        })
+        .state({
+            name: 'mypage',
+            url: '/mypage',
+            views: {
+                'mainbody': {
+                    templateUrl: 'mypage.html'
+                }
             }
-        }
-    })
-    .state({
-        name: 'notification',
-        url: '/',
-                views: {
-            'mainbody': {
-                template: '<h3>notifications placed in dropdown form</h3>'
+        })
+        .state({
+            name: 'notification',
+            url: '/',
+            views: {
+                'mainbody': {
+                    template: '<h3>notifications placed in dropdown form</h3>'
+                }
             }
-        }
-    })
-    .state({
-        name: 'setting',
-        url: '/setting',
-        views: {
-            'mainbody': {
-                template: '<h3>settings</h3>'
+        })
+        .state({
+            name: 'setting',
+            url: '/setting',
+            views: {
+                'mainbody': {
+                    template: '<h3>settings</h3>'
+                }
             }
-        }
-    })
-    .state({
-        name: 'help',
-        url: '/',
-        views: {
-            'mainbody': {
-                template: '<h3>help popup on homepage</h3>'
+        })
+        .state({
+            name: 'help',
+            url: '/',
+            views: {
+                'mainbody': {
+                    template: '<h3>help popup on homepage</h3>'
+                }
             }
-        }
-    })
+        })
 
 });
 
 app.controller('meetingCtrl', function ($scope, $log) {
     $scope.meetings = [
         {
-            "name": "E3 Frisbee",
+            "name": "N1 Puppies",
             "user_id": "1",
             "latitude": "36.37425202630263",
             "longitude": "127.36579723271618",
             "date": "2020-06-13",
             "time": "14:30",
-            "people": "3",
-            "distance": "406m",
+            "people": 3,
+            "distance": "406",
+            "joined": false,
         },
         {
-            "name": "N1 Puppies",
+            "name": "E3 Frisbee",
             "user_id": "1",
             "latitude": "36.368031596219836",
             "longitude": "127.36540920346589",
             "date": "2020-06-14",
             "time": "19:30",
-            "people": "2",
-            "distance": "30m",
+            "people": 2,
+            "distance": "30",
+            "joined": false,
         },
         {
             "name": "N5 Come and play!",
@@ -148,12 +150,13 @@ app.controller('meetingCtrl', function ($scope, $log) {
             "longitude": "127.3644810196351",
             "date": "2020-06-17",
             "time": "10:30",
-            "people": "2",
-            "distance": "160m",
+            "people": 2,
+            "distance": "160",
+            "joined": false,
         },
     ]
 
-    $scope.time = new Date();
+    $scope.formTime = new Date();
     $scope.hstep = 1;
     $scope.mstep = 10;
 
@@ -163,16 +166,16 @@ app.controller('meetingCtrl', function ($scope, $log) {
     };
 
     $scope.changed = function () {
-        $log.log('Time changed to: ' + $scope.mytime);
+        $log.log('Time changed to: ' + $scope.formTime);
     };
 
     $scope.today = function () {
-        $scope.dt = new Date();
+        $scope.formDate = new Date();
     };
     $scope.today();
 
     $scope.clear = function () {
-        $scope.dt = null;
+        $scope.formDate = null;
     };
 
     $scope.inlineOptions = {
@@ -203,7 +206,7 @@ app.controller('meetingCtrl', function ($scope, $log) {
     };
 
     $scope.setDate = function (year, month, day) {
-        $scope.dt = new Date(year, month, day);
+        $scope.formDate = new Date(year, month, day);
     };
 
     $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
@@ -229,24 +232,31 @@ app.controller('meetingCtrl', function ($scope, $log) {
         }
     ];
 
-    $scope.submit = function() {
+    $scope.submit = function () {
         var name = document.getElementById('name').value;
-        console.log(formatDate($scope.dt))
-        console.log($scope.time)
-        if (name != "" && marker != undefined) {
+        if (name != "" && createMarker != undefined) {
             var new_event = {
                 "name": name,
                 "user_id": "1",
-                "latitude": marker.position.lat(),
-                "longitude": marker.position.lat(),
-                "date": formatDate($scope.dt),
-                "time": String($scope.time).substring(16, 21),
+                "latitude": createMarker.position.lat(),
+                "longitude": createMarker.position.lng(),
+                "date": formatDate($scope.formDate),
+                "time": String($scope.formTime).substring(16, 21),
                 "people": "1",
-                "distance": "???m",
+                "distance": String(latLngToDistance(createMarker.position.lat(), createMarker.position.lng())),
+                "joined": true,
             }
             $scope.meetings.push(new_event);
-            new google.maps.Marker({ position: marker.position, map: showMap });
+            $log.log($scope.meetings)
+            var newMarker = new google.maps.Marker({ position: createMarker.position, map: showMap });
+            newMarker.addListener('click')
         }
+    }
+
+    function latLngToDistance(lat, lng) {
+        var n1_lat = 36.368031596219836
+        var n1_lng = 127.36540920346589
+        return Math.round(Math.sqrt((n1_lat - lat) ** 2 + (n1_lng - lng) ** 2) * 111000)
     }
 
     function formatDate(date) {
@@ -254,17 +264,17 @@ app.controller('meetingCtrl', function ($scope, $log) {
             month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
             year = d.getFullYear();
-    
-        if (month.length < 2) 
+
+        if (month.length < 2)
             month = '0' + month;
-        if (day.length < 2) 
+        if (day.length < 2)
             day = '0' + day;
-    
+
         return [year, month, day].join('-');
     }
 
 
-    var createMap, showMap, marker;
+    var createMap, showMap, listMap, createMarker, listMarker;
     $scope.$on('mapInitialized', function (event, evtMap) {
         if (evtMap.id == "create-map") {
             createMap = evtMap;
@@ -274,25 +284,75 @@ app.controller('meetingCtrl', function ($scope, $log) {
         } else if (evtMap.id == "show-map") {
             showMap = evtMap;
             $scope.meetings.forEach(placeMarker);
+        } else if (evtMap.id == "list-map") {
+            listMap = evtMap;
         }
     })
 
     function refreshMarker(location) {
-        if (marker != undefined) {
-            marker.setMap(null);
+        if (createMarker != undefined) {
+            createMarker.setMap(null);
         }
-        marker = new google.maps.Marker({
+        createMarker = new google.maps.Marker({
             position: location,
             map: createMap,
+            animation: google.maps.Animation.BOUNCE,
         });
-        var infowindow = new google.maps.InfoWindow({
-            content: 'Here?'
-        });
-        infowindow.open(createMap, marker);
+        window.setTimeout(function () {
+            createMarker.setAnimation(null);
+        }, 3000);
+        // var infowindow = new google.maps.InfoWindow({
+        //     content: 'Here?'
+        // });
+        // infowindow.open(createMap, createMarker);
     }
 
     function placeMarker(meeting, index, array) {
-        var latLng = new google.maps.LatLng(Number(meeting["latitude"]), Number(meeting["longitude"]));
-        new google.maps.Marker({ position: latLng, map: showMap });
+        var latLng = new google.maps.LatLng(Number(meeting.latitude), Number(meeting.longitude));
+        var mapMarker = new google.maps.Marker({ position: latLng, map: showMap });
+        var infowindow = new google.maps.InfoWindow({
+            content: 'Meeting Name: ' + meeting.name + '<br>Date: ' + meeting.date + '<br>Time: ' + meeting.time
+        });
+        mapMarker.addListener('click', function () {
+            infowindow.open(showMap, mapMarker);
+        });
+    }
+
+    $scope.detailMeetingName = "E3 Frisbee";
+    $scope.detailMeetingDate = "2020-06-13";
+    $scope.detailMeetingTime = "14:30";
+    $scope.detailMeetingJoined = false;
+
+    $scope.showMeeting = function (meeting) {
+        $scope.detailMeetingName = meeting.name;
+        $scope.detailMeetingDate = meeting.date;
+        $scope.detailMeetingTime = meeting.time;
+        if (listMarker != undefined) {
+            listMarker.setMap(null);
+        }
+        var latLng = new google.maps.LatLng(Number(meeting.latitude), Number(meeting.longitude));
+        listMap.setCenter(latLng);
+        listMarker = new google.maps.Marker({
+            position: latLng,
+            map: listMap,
+            animation: google.maps.Animation.DROP,
+        });
+        $scope.detailMeetingJoined = meeting.joined;
+        renderJoinBtn();
+    }
+
+    $scope.toggleJoin = function () {
+        $scope.detailMeetingJoined = !$scope.detailMeetingJoined;
+        renderJoinBtn();
+        $log.log($scope.detailMeetingJoined)
+    }
+
+    function renderJoinBtn() {
+        var joinBtn = document.getElementById("toggleJoin");
+        if ($scope.detailMeetingJoined) {
+            joinBtn.innerHTML = '<label class="btn btn-success"> <input type="checkbox" ng-checked="detailMeetingJoined" autocomplete="off"> Joined</label>'
+        } else {
+            joinBtn.innerHTML = '<label class="btn btn-info"> <input type="checkbox" ng-checked="detailMeetingJoined" autocomplete="off"> Join  </label>'
+        }
     }
 });
